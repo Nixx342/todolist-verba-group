ee146516a5434686bfe1114523154ac26fec0a29
Maksim Sytnik <sytnik.m.yu@gmail.com>
2024-10-31 17:59:10 +0500

    1

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,22 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
-// Пример компонентов страниц
 import HomePage from './Pages/HomePage';
 import LoginPage from './Pages/LoginPage';
 
 function App() {
-  // Переменная для статуса авторизации
   const [isLogin, setIsLogin] = useState(false);
 
-  // Проверка статуса авторизации при монтировании компонента
   useEffect(() => {
     const loginStatus = localStorage.getItem('isLogin') === 'true';
     setIsLogin(loginStatus);
   }, []);
 
-  // Функция для обработки авторизации
   const handleLogin = () => {
     setIsLogin(true);
     localStorage.setItem('isLogin', 'true');
   };
 
-  // Функция для выхода из системы
   const handleLogout = () => {
     setIsLogin(false);
     localStorage.setItem('isLogin', 'false');
